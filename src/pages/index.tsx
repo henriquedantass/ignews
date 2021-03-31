42711cf8c0fad1eae45ca0b304ec5ade9b3b9245
henrique dantas <paulo.henrique@sapienstec.com>
2021-03-30 22:38:01 -0300

    global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | IgNews</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
+  )
+}

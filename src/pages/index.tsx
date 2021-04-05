4b0764511612956736ce2668720fd983759e1a65
henrique dantas <paulo.henrique@sapienstec.com>
2021-04-05 20:56:20 -0300

    button subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import React from 'react';
 import styles from './home.module.scss';
+import {SubscribeButton} from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -15,6 +17,7 @@ export default function Home() {
             Get acess to all the publications <br/>
             <span>For $9,90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
 
         <img src='/images/avatar.svg' alt='girl codding'></img>

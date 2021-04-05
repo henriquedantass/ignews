9c249c5b874cf9c42a1b54d8ac934d4927d913e1
henrique dantas <paulo.henrique@sapienstec.com>
2021-04-05 20:50:24 -0300

    home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | IgNews</title>
+        <title>Home | IgNews</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get acess to all the publications <br/>
+            <span>For $9,90 month</span>
+          </p>
+        </section>
+
+        <img src='/images/avatar.svg' alt='girl codding'></img>
+
+      </main>
     </>
   )
 }

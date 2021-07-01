0419b834eb4efc0d69e5c66a7a0b3c1bdba0f18a
henrique dantas <paulo.henrique@sapienstec.com>
2021-07-01 13:31:56 -0300

    adicionando subscripcion no faunadb

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,13 @@
+import { query as q} from 'faunadb'
+import { fauna } from '../../../services/fauna';
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string
+){
+    console.log(subscriptionId, customerId , 'hello world')
+
+    
+
+}
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,8 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe'
+import { saveSubscription} from "./_lib/manageSubscription";
 
-async function buffer(readable:Readable){
+async function buffer(readable:Readable){ 
     const chunks = []
 
     for await (const chunk of readable) {
@@ -39,7 +40,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
        const type = event.type;
 
        if(relevantEvents.has(type)) {
-            console.log('Evento recebido', event)
+           try {
+               switch(type) {
+                   case 'checkout.session.completed':
+                       const checkoutSession = event.data.object as Stripe.Checkout.Session 
+                       
+                       await saveSubscription(
+                           checkoutSession.subscription.toString(),
+                           checkoutSession.customer.toString(),
+                       )
+                    break;
+                   default :
+                    throw new Error('Unhandled event') 
+                   
+               }
+           } catch(err) {
+                return res.json({error: 'Webhook handler failed'})
+       }
        }
 
        res.json({ok: true})

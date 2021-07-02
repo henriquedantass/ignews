979fde434fecbb22c7dc45b125ba7c83cd6db8bc
henrique dantas <paulo.henrique@sapienstec.com>
2021-07-02 11:55:44 -0300

    consumindo api do prismic

---
+++ src/services/prismic.ts
@@ -0,0 +1,13 @@
+import Prismic from '@prismicio/client'
+
+
+export function getPrismicClient(req?: unknown){
+    const prismic = Prismic.client(
+        process.env.PRISMIC_API_URL,
+        {
+            req,
+            accessToken: process.env.PRISMIC_ACESS_TOKEN,
+        }
+    )
+    return prismic
+}
